ef7dfdcbd1f79b5e5620539f6dcfa4bcfa048dcb
Nick Muravev <murvievnick@gmail.com>
2024-11-26 12:16:01 +0300

    fix: правка багов

--- src/js/app.js
+++ src/js/app.js
@@ -22,3 +22,47 @@ if(document.querySelector('[data-slider="main-news"]')) {
     }
   })
 }
+
+if (document.querySelector('[data-map]')) {
+  const mapScript = document.createElement("script");
+  mapScript.src =
+    "https://api-maps.yandex.ru/2.1/?apikey=ваш API-ключ&lang=ru_RU";
+
+  document.body.append(mapScript);
+
+  mapScript.addEventListener("load", () => {
+    ymaps.ready(init);
+
+    function init() {
+      let myMap = new ymaps.Map("map", {
+        center: [55.189789, 61.287821], // координаты центра Москвы
+        zoom: 16
+      });
+  
+      var myPlacemark = new ymaps.Placemark([55.189789, 61.287821], {
+        balloonContent: "Центр Москвы"
+      });
+  
+      myMap.geoObjects.add(myPlacemark);
+
+      myMap.behaviors.disable('scrollZoom') 
+    }
+  });
+}
+
+const headerMenu = document.querySelector('.js-header-menu');
+const headerMenuBtn = document.querySelector('.js-header-menu-btn');
+
+if(headerMenu && headerMenuBtn) {
+  const headerMenuClose = headerMenu.querySelector('.js-header-menu-close');
+
+  headerMenuBtn.addEventListener('click', () => {
+    headerMenu.classList.add('is-open');
+    headerMenuBtn.classList.add('is-active');
+  })
+
+  headerMenuClose.addEventListener('click', () => {
+    headerMenu.classList.remove('is-open');
+    headerMenuBtn.classList.remove('is-active');
+  })
+}
